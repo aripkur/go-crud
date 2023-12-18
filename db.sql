f14e34be61aa0d9df502c3d6afeb90a7dec17068
arif kurniawan <arifkur10027@gmail.com>
2023-12-18 12:31:37 +0700

    first commit

---
+++ db.sql
@@ -0,0 +1,11 @@
+-- create database go_crud
+-- use go_crud
+
+-- create table category (
+--     id integer primary key auto_increment,
+--     name varchar(200) not null
+-- ) engine = innoDB;
+
+desc category
+
+-- select * from category
